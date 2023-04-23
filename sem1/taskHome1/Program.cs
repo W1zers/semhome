ff0374d72666eef6ba42cf75affededf5c73736b
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-04-24 01:56:30 +0300

    added homesem1

---
+++ sem1/taskHome1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a , b;
+
+Console.WriteLine("Введите число A: ");
+
+    a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+
+    b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+{
+     Console.WriteLine($"max = {a}");
+}
+if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else Console.WriteLine("Оба числа равны");
---
+++ sem1/taskHome2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел:
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите 1-ое число: ");
+
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2-ое число: ");
+
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 3-е число: ");
+
+int n3 = int.Parse(Console.ReadLine());
+
+if (n1 > n2 & n1 > n3)
+{
+    Console.WriteLine($"max = {n1}");
+}
+else if (n2 > n1 & n2 > n3)
+{
+    Console.WriteLine($"max = {n2}");
+}
+
+else if (n3 > n1 & n3 > n2)
+{
+    Console.WriteLine($"max = {n3}");
+}
+else
+{
+Console.WriteLine("Числа равны");
+}
