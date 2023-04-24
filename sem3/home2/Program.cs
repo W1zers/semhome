188303079b293acc47e555d55cde9c7859edd402
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-04-24 15:35:35 +0300

    added home2/3

--- sem3/home2/Program.cs
+++ sem3/home2/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = ReadInt("Введите координату X1 первой точки: ");
+int y1 = ReadInt("Введите координату Y1 первой точки: ");
+int z1 = ReadInt("Введите координату Z1 первой точки: ");
+int x2 = ReadInt("Введите координату X2 второй точки: ");
+int y2 = ReadInt("Введите координату Y2 второй точки: ");
+int z2 = ReadInt("Введите координату Z2 второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
