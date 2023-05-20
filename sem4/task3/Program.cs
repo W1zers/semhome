ae8f8bf1be2e6d2886355ad3aa9c595e437e7921
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-05-21 01:11:50 +0300

    add info

---
+++ sem4/task3/Program.cs
@@ -0,0 +1,124 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message)
+{
+        System.Console.Write(massage);
+        string ReadInput = System.Console.ReadLine();
+        int result = int.Parse(ReadInput);
+        return result;
+}
+
+int[] GenerateArry(int Length, int minValue, int maxValue);
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArry(int [] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.length; i++)
+    {
+        System.Console.Write($"{array [i] }, ");
+    }
+    System.Console.Write($"{array [array.Length - 1]}");
+    System.Console.WriteLine("]");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+int max = Prompt("Конечное значаение, для диапазона случайного числа: ");
+int[] array = GenerateArry(length, min, max);
+
+PrintArry(array);
+
+
+
+// Console.WriteLine($"\nЗадача 29. Ряд чисел преобразует в массив");
+// Console.Write("Введите ряд чисел, разделенных запятой : ");
+// string? seriesOfNumbers = Console.ReadLine();
+
+// seriesOfNumbers = seriesOfNumbers + ",";    // дополнительня запятая для обозначения конца строки
+
+// // функция удаления пробелов из строки 
+// string RemovingSpaces (string series){
+//   string seriesNew = "";
+//   for (int i = 0; i < series.Length; i++)
+//   {
+//     if (series[i] != ' ') 
+//     {
+//       seriesNew += series[i];
+//     }
+//   }
+//   return seriesNew;
+// }
+
+// //  функция  проверки на правильность ввода 
+// void СheckNumber2 (int  series){
+
+//       if (series == '0'||series == '1'||series == '2'
+//       ||series == '3'||series == '4'||series == '5'||series == '6'
+//       ||series == '7'||series == '8'||series == '9'||series == ','
+//       ||series == '-')
+//       {
+//       }
+//         else {
+//           Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
+
+//       }
+// }
+
+// // функция  создания и заполнения массива из строки
+// int[] ArrayOfNumbers(string seriesNew){ 
+
+//   int[] arrayOfNumbers = new int[1];    // инициализация массива из 1 элемента
+
+//   int j =0;
+
+//   for (int i = 0; i < seriesNew.Length; i++){
+//     string seriesNew1 = "";
+
+//     while (seriesNew[i] != ',' && i < seriesNew.Length){
+//       seriesNew1 += seriesNew[i];
+//       СheckNumber2(seriesNew[i]);
+//       i++;
+//     }
+//     arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
+//     if (i < seriesNew.Length-1){
+//       arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    // добавляет новый нулевой элемент в конец массива
+//     }
+//     j++;
+//   }
+//   return arrayOfNumbers;
+// }
+
+// // функция  вывода массива на печать 
+// void PrintArry(int[] coll){
+//   int count = coll.Length;
+//   int index = 0;
+//   Console.Write("[");
+//   while(index < count){
+//     Console.Write(coll[index]);
+//     index++;
+//     if (index < count){
+//       Console.Write(", ");
+//     }
+//   }
+//   Console.Write("]");
+// } 
+
+
+// string seriesNew = RemovingSpaces(seriesOfNumbers);
+
+// int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+
+// PrintArry(arrayOfNumbers);
